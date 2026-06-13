b8d009fc957276e8f6e7cc8eb8f3e51dadf85070
Prashant <prashantkumar2531@gmail.com>
2026-06-13 16:50:50 +0530

    create project and add conversion request model

---
+++ UnitConversionApi/UnitConversionApi/Model/ConversionRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace UnitConversionApi.Api.Models
+{
+    public class ConversionRequest
+    {
+        public string Category
+        {
+            get; set;
+
+        } = string.Empty;
+
+        public string FromUnit
+        {
+            get; set;
+        } = string.Empty;
+
+        public string ToUnit
+        {
+            get; set;
+        } = string.Empty;
+
+        public double Value
+        {
+            get; set;
+        }
+    }
+}
