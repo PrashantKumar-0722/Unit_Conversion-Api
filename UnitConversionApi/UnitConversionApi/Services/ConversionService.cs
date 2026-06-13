4848e2e1acc5b5ff4b5a29935f3f204d7a97b94f
Prashant <prashantkumar2531@gmail.com>
2026-06-13 21:36:12 +0530

    Implement conversion Service

---
+++ UnitConversionApi/UnitConversionApi/Services/ConversionService.cs
@@ -0,0 +1,30 @@
+﻿using UnitConversionApi.Api.Converters;
+
+namespace UnitConversionApi.Api.Services;
+
+public class ConversionService
+{
+    private readonly Dictionary<string, IUnitConverter> _converters;
+
+    public ConversionService(IEnumerable<IUnitConverter> converters)
+    {
+        _converters = converters.ToDictionary(
+            c => c.Category.ToLower());
+    }
+
+    public double Convert(
+        string category,
+        double value,
+        string fromUnit,
+        string toUnit)
+    {
+        if (!_converters.ContainsKey(category.ToLower()))
+        {
+            throw new ArgumentException(
+                $"Category '{category}' is not supported.");
+        }
+
+        return _converters[category.ToLower()]
+            .Convert(value, fromUnit, toUnit);
+    }
+}
