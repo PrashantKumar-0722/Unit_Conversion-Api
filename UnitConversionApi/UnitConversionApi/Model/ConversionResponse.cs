e64a11512020b948892f96d0245fc69cfdae427e
Prashant <prashantkumar2531@gmail.com>
2026-06-13 21:03:17 +0530

    Conversion Response Model created

---
+++ UnitConversionApi/UnitConversionApi/Model/ConversionResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace UnitConversionApi.Api.Models;
+
+public class ConversionResponse
+{
+    public double OriginalValue
+    { 
+        get; set; 
+            }
+    public string FromUnit 
+    { 
+        get; set; 
+    } = string.Empty;
+    public string ToUnit
+    { 
+        get; set; 
+    } = string.Empty;
+    public double ConvertedValue 
+    { get; set; 
+    }
+}
